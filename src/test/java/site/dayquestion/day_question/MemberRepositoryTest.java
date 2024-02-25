fb67f116e27e52a02ae68730b59d35a4522635ec
Yeji Kim <kimpizzzaa@gmail.com>
2024-02-25 16:08:56 +0900

    refactor: Member API 구현

--- src/test/java/site/dayquestion/day_question/MemberRepositoryTest.java
+++ src/test/java/site/dayquestion/day_question/MemberRepositoryTest.java
@@ -6,32 +6,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import site.dayquestion.domain.Member;
-import site.dayquestion.repository.MemberRepository;
+import site.dayquestion.Member.repository.MemberRepository;
+import java.util.List;
 
-//@ExtendWith(SpringExtension.class)
+
 @SpringBootTest
 class MemberRepositoryTest {
-
-
     @Autowired
-    MemberRepository memberRepository;
+    MemberRepository memRep;
 
     @Test
     @Transactional
-    public void testMember() throws Exception {
-        //given: 이런게 주어졌을떄
-        Member member = new Member();
-        member.setNickname("memberA");
+    public void 회원가입_및_찾기_테스트(){
+        Member member = Member.builder()
+                .nickname("멤버1")
+                .email("wedwedw@wer3.com")
+                .password("1234").build();
 
-        //when: 이렇게하면 (테스트 하고싶은 내용)
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
+        Long saveId = memRep.save(member);
+        Member findMember = memRep.findMemberById(saveId);
 
-        //then: 이렇게된다 검증해라
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getNickname()).isEqualTo(member.getNickname());
 
     }
+    @Test
+    @Transactional
+    public void 랜덤회원3명_찾기_테스트(){
+        Member member1 = Member.builder()
+                .nickname("멤버1")
+                .email("wedwedw@wer3.com")
+                .password("1234").build();
+        Member member2 = Member.builder()
+                .nickname("멤버2")
+                .email("wedwedw@wer3.com")
+                .password("1234").build();
+        Member member3 = Member.builder()
+                .nickname("멤버3")
+                .email("wedwedw@wer3.com")
+                .password("1234").build();
+        Member member4 = Member.builder()
+                .nickname("멤버4")
+                .email("wedwedw@wer3.com")
+                .password("1234").build();
+        Member member5 = Member.builder()
+                .nickname("멤버5")
+                .email("wedwedw@wer3.com")
+                .password("1234").build();
+        memRep.save(member1);
+        memRep.save(member2);
+        memRep.save(member3);
+        memRep.save(member4);
+        memRep.save(member5);
 
+        List<Member> memberList = memRep.findMemberList(3);
+        for (int i = 0; i < memberList.size(); i++) {
+            System.out.println(memberList.get(i).getNickname());
+        }
+
+    }
 
 }
